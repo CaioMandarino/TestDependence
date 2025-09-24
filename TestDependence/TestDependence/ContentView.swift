b498ce325b613be804a1b44b2107403877fe27df
Caio <caiomand04@gmail.com>
2025-09-24 10:56:31 -0300

    pronto

---
+++ TestDependence/TestDependence/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestDependence
+//
+//  Created by Caio Mandarino on 24/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestDependence/TestDependence/TestDependenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDependenceApp.swift
+//  TestDependence
+//
+//  Created by Caio Mandarino on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TestDependenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
